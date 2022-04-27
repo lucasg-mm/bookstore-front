74558de266b19929c18c03c2cad29a782263aad4
Lucas Miranda <lucasgmm@usp.br>
2022-04-27 09:56:45 -0300

    Added DELETE categoria

--- src/app/components/views/categoria/categoria.service.ts
+++ src/app/components/views/categoria/categoria.service.ts
@@ -1,35 +1,44 @@
-import { validateVerticalPosition } from '@angular/cdk/overlay';
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
-import { Categoria } from './categoria-read/categoria.model';
+import { validateVerticalPosition } from "@angular/cdk/overlay";
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+import { Categoria } from "./categoria-read/categoria.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CategoriaService {
-
   baseUrl: String = environment.baseUrl;
 
-  constructor(private http: HttpClient, private _snack: MatSnackBar) {  }
+  constructor(private http: HttpClient, private _snack: MatSnackBar) {}
 
-  findAll(): Observable<Categoria[]>{
-    const url  = `${this.baseUrl}/categorias`;
+  findAll(): Observable<Categoria[]> {
+    const url = `${this.baseUrl}/categorias`;
     return this.http.get<Categoria[]>(url);
   }
 
-  create(categoria: Categoria): Observable<Categoria>{
+  findById(id: String): Observable<Categoria> {
+    const url = `${this.baseUrl}/categorias/${id}`;
+    return this.http.get<Categoria>(url);
+  }
+
+  create(categoria: Categoria): Observable<Categoria> {
     const url = `${this.baseUrl}/categorias`;
     return this.http.post<Categoria>(url, categoria);
   }
 
-  mensagem(str: String): void{
-    this._snack.open(`${str}`, 'OK', {
-      horizontalPosition: 'end',
-      verticalPosition: 'top',
-      duration: 3000
-    })
+  delete(id: String): Observable<void>{
+    const url = `${this.baseUrl}/categorias/${id}`;
+    return this.http.delete<void>(url);
+  }
+
+  mensagem(str: String): void {
+    this._snack.open(`${str}`, "OK", {
+      horizontalPosition: "end",
+      verticalPosition: "top",
+      duration: 3000,
+    });
   }
 }
