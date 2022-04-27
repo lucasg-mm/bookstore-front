8f89844f271050f885435424b5b08f95f212d7eb
Lucas Miranda <lucasgmm@usp.br>
2022-04-27 11:21:57 -0300

    Added UPDATE for 'categoria'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { MatInputModule } from "@angular/material/input";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { CategoriaDeleteComponent } from './components/views/categoria/categoria-delete/categoria-delete.component';
+import { CategoriaUpdateComponent } from './components/views/categoria/categoria-update/categoria-update.component';
 
 @NgModule({
   declarations: [
@@ -34,6 +35,7 @@ import { CategoriaDeleteComponent } from './components/views/categoria/categoria
     CategoriaReadComponent,
     CategoriaCreateComponent,
     CategoriaDeleteComponent,
+    CategoriaUpdateComponent,
   ],
   imports: [
     BrowserModule,
